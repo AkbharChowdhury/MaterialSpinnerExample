2f46495266ea910f975efc6df666fd558a25081d
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-01-16 20:03:22 +0000

    a

--- app/src/main/java/com/myapp/materialspinnerexample/MainActivity2.java
+++ app/src/main/java/com/myapp/materialspinnerexample/MainActivity2.java
@@ -11,19 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.myapp.materialspinnerexample.databinding.ActivityMainBinding;
+import com.myapp.materialspinnerexample.databinding.ActivityMain2Binding;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        var binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         EdgeToEdge.enable(this);
-//        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -31,24 +32,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        var binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
         List<String> data = Arrays.stream(getResources().getStringArray(R.array.tags_array)).sorted().collect(Collectors.toList());
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.dropdown_item, data);
+        AutoCompleteTextView autoCompleteTextViewMeal = binding.autoCompleteTextViewMeal;
 
-        AutoCompleteTextView autoCompleteMeals = binding.autoCompleteMeals;
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_item, data);
-        autoCompleteMeals.setAdapter(adapter);
-        setDropDownIndex(autoCompleteMeals, 0);
-        autoCompleteMeals.setOnItemClickListener((parent, view, position, id) -> {
+        autoCompleteTextViewMeal.setAdapter(arrayAdapter);
+        autoCompleteTextViewMeal.setText(autoCompleteTextViewMeal.getAdapter().getItem(0).toString(), false);
+        autoCompleteTextViewMeal.setOnItemClickListener((parent, view, position, id) -> {
             String selectedMeal = parent.getAdapter().getItem(position).toString();
             Toast.makeText(this, selectedMeal, Toast.LENGTH_SHORT).show();
-//            setDropDownIndex(autoCompleteMeals, position);
         });
     }
 
-    private void setDropDownIndex(AutoCompleteTextView autoCompleteTextView, int index) {
-        autoCompleteTextView.setText(
-                autoCompleteTextView.getAdapter().getItem(index).toString(), false);
-    }
 }

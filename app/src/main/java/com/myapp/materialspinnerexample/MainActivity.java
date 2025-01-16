aa2c190cb08435cecedc0e8e5ff7c5f0db989652
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-01-16 16:04:20 +0000

    Initial commit

---
+++ app/src/main/java/com/myapp/materialspinnerexample/MainActivity.java
@@ -0,0 +1,51 @@
+package com.myapp.materialspinnerexample;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.myapp.materialspinnerexample.databinding.ActivityMainBinding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        var binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<String> data = Arrays.stream(getResources().getStringArray(R.array.tags_array)).sorted().collect(Collectors.toList());
+
+        AutoCompleteTextView autoCompleteMeals = binding.autoCompleteMeals;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_item, data);
+        autoCompleteMeals.setAdapter(adapter);
+        autoCompleteMeals.setText(adapter.getItem(0), false);
+
+        autoCompleteMeals.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedMeal = parent.getAdapter().getItem(position).toString();
+            Toast.makeText(this, selectedMeal, Toast.LENGTH_SHORT).show();
+            autoCompleteMeals.setText(adapter.getItem(position), false);
+
+        });
+    }
+}
